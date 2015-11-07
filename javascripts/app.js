c31ecf960e321e96a87036ef2b870dacf90e0d06
jonathanhinton <hintonjonathan@gmail.com>
2015-11-07 10:29:03 -0600

    initial setup

---
+++ javascripts/app.js
@@ -0,0 +1,21 @@
+/* jshint esnext: true */
+requirejs.config({
+  baseUrl: "./javascripts",
+  paths:{
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
+    "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
+    "firebase" : "../lib/bower_components/firebase/firebase",
+    "lodash" : "../lib/bower_components/lodash/lodash.min",
+    "material": "../lib/bower_components/bootstrap-material-design/dist/js/material.min",
+    "noUiSlider": "../lib/noUiSlider.8.0.2/nouislider.min",
+    "q" : "../lib/bower_components/q/q"
+  },
+  shim: {
+    "bootstrap": ["jquery"],
+    "material": ["bootstrap"],
+    "noUiSlider": ["jquery"],
+    "firebase" : {exports: "Firebase"
+    }
+  }
+});
