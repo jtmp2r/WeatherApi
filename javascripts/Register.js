3683ce378319dd0307c1a97d0f38fca6f83940db
jonathanhinton <hintonjonathan@gmail.com>
2015-11-07 12:56:30 -0600

    fix indentation/syntax

--- javascripts/Register.js
+++ javascripts/Register.js
@@ -1,26 +1,22 @@
 define(["jquery", "lodash", "firebase"], function($, lodash, Firebase) {
 
-	return {
-   var newUser: function() {
-    var	newUserEm = $("#").val();
-    var newUserPas = $("#").val();
+  return {
+    newUser : function() {
+      var	newUserEm = $("#").val();
+      var newUserPas = $("#").val();
 
-    var ref = new Firebase(https://yoreweather.firebaseio.com/)
+      var ref = new Firebase("https://yoreweather.firebaseio.com/");
 
     ref.createUser ({
-    	email: newUserEm,
-    	password: newUserPas
+      email: newUserEm,
+      password: newUserPas
     }, function(error, userData) {
-    	if (error){
-    		console.log("Error creating user ", error);
-    	} else {
-    		console.log("YOu have been born " userData.uid)
-    	}
-    }
-    ); // end of createUser
-
-    }
-
-	}
-	
+        if (error){
+          console.log("Error creating user ", error);
+        } else {
+          console.log("You have been born", userData.uid);
+        }
+      }); // end of createUser
+    } //end newUser
+  }; //end return statement
 });
--- javascripts/login.js
+++ javascripts/login.js
@@ -1,24 +1,23 @@
-define(["jquery", ], function($) {
+define(["jquery"], function($) {
 
-	var ref = new Firebase(https://yoreweather.firebaseio.com/)
+	var ref = new Firebase("https://yoreweather.firebaseio.com/");
 
   return {
-  	loginUser: function() {
-  		newEmail = $("#").val();
-  		newPassword = $("#").val();
+    loginUser: function() {
+      newEmail = $("#").val();
+      newPassword = $("#").val();
 
-  	ref.authWithPassword({
-  		email: newEmail,
-  		password: newPassword
-  	}, function(error, authData) {
-  		if (error) {
-  			console.log("Didn't work", error)
-		}, else {
-			  console.log("Authenticated successfully with payload:", authData);
-		}
-  	}
-  	);
-   }
-  }  //return ending
-	
+    ref.authWithPassword({
+      email: newEmail,
+      password: newPassword
+    }, function(error, authData) {
+        if (error) {
+          console.log("Didn't work", error);
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+        }
+      });
+    } //end loginUser
+  };  //return ending
+
 });
